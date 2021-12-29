507112b3f255e0b1f5a8966a8762075337b20208
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-12-29 19:13:16 +0100

    Refactor

---
+++ src/components/illustrations/IllustrationSkeleton.js
@@ -0,0 +1,23 @@
+import { Box, Grid, Skeleton } from "@mui/material";
+
+/**
+ * @param {IllustrationProps}
+ */
+export default function IllustrationSkeleton({ boxProps }) {
+  return (
+    <Box {...boxProps}>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Skeleton 
+          animation="wave"
+          height={200}
+          width={128}
+        />
+      </Grid>
+    </Box>
+  );
+}
