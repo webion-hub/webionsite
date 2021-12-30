4e708d448fd94b4134d2d4768625358007ae51a7
Stefano Calabretti <45517710+cala-br@users.noreply.github.com>
2021-12-30 18:30:12 +0100

    Add skeleton props (#15)
    * Center skeleton
    
    * Add typing
    
    * Refactor
    
    * Add endline
    
    * Add endline
    
    * Use interfaces for typing
    
    * Add skeleton props

--- src/components/illustrations/IllustrationSkeleton.js
+++ src/components/illustrations/IllustrationSkeleton.js
@@ -3,7 +3,10 @@ import { Box, Grid, Skeleton } from "@mui/material";
 /**
  * @param {import("./typing").IllustrationProps}
  */
-export default function IllustrationSkeleton({ boxProps }) {
+export default function IllustrationSkeleton({ boxProps, skeletonProps }) {
+  const size =
+    getSkeletonSize(skeletonProps.size);
+
   return (
     <Box {...boxProps}>
       <Grid
@@ -12,12 +15,33 @@ export default function IllustrationSkeleton({ boxProps }) {
         justifyContent="center"
         alignItems="center"
       >
-        <Skeleton 
-          animation="wave"
-          height={200}
-          width={128}
-        />
+        <Skeleton animation="wave" {...size}/>
       </Grid>
     </Box>
   );
+}
+
+/**
+ * @param {SkeletonSize} size
+ * @returns {Size}
+ */
+const getSkeletonSize = (size) => {
+  const normalWidth = 128;
+  const normalHeight = 200;
+  const result = {
+    'small': {
+      width: normalWidth * 0.5,
+      height: normalHeight * 0.5,
+    },
+    'normal': {
+      width: normalWidth,
+      height: normalHeight,
+    },
+    'large': {
+      width: normalWidth * 1.5,
+      height: normalHeight * 1.5,
+    },
+  }[size];
+
+  return result ?? size;
 }
