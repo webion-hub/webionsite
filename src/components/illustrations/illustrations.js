f8c8ce6a45093eb4ccbf64e3b379f8aed9f5ce5a
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-01-02 19:02:50 +0100

    Rename icons folder

--- src/components/illustrations/illustrations.js
+++ src/components/illustrations/illustrations.js
@@ -4,6 +4,6 @@ export const Programming = Illustration.lazy('illustrations/programming.svg');
 export const Personalization = Illustration.lazy('illustrations/personalization.svg');
 export const Speed = Illustration.lazy('illustrations/speed2.svg');
 
-export const LogoIcon = Illustration.icon("svgs/logo.svg");
-export const LinkedinIcon = Illustration.icon("svgs/linkedin.svg");
-export const InstagramIcon = Illustration.icon("svgs/instagram.svg");
+export const LogoIcon = Illustration.icon("icons/logo.svg");
+export const LinkedinIcon = Illustration.icon("icons/linkedin.svg");
+export const InstagramIcon = Illustration.icon("icons/instagram.svg");
