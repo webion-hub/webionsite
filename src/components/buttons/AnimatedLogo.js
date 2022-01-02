1c3aea71a5b529c81a4c5bfb6b6be12833bff6bf
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-01-02 20:14:59 +0100

    Display skeleton correctly

--- src/components/buttons/AnimatedLogo.js
+++ src/components/buttons/AnimatedLogo.js
@@ -7,19 +7,19 @@ export default function AnimatedLogo({label, ...others}) {
       {...others}
       sx={{
         paddingRight: 1,
-        "& span": {
+        "& span.webion-logo--label": {
           paddingLeft: 1,
           transition: `${theme.transitions.duration.short}ms all`,
           opacity: 0,
           transform: "translate(-100%, 0%)",
         },
-        "&:hover > span": {
+        "&:hover > span.webion-logo--label": {
           opacity: 1,
           transform: "translate(0%, 0%)",
         },
       }}
     >
-      <span>
+      <span className='webion-logo--label'>
         {label}
       </span>
     </Logo>
