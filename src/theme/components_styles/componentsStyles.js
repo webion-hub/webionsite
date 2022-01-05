fb1308082a578778f7c71b9069f45fd0b651c052
matteo2437 <matteo.budriesi@gmail.com>
2022-01-05 16:11:11 +0100

    update theme

--- src/theme/components_styles/componentsStyles.js
+++ src/theme/components_styles/componentsStyles.js
@@ -1,8 +1,10 @@
 import appBarStyle from "./appbarStyle"
 import cssBaseline from "./css_baseline/cssBaseline"
 import drawerStyle from "./drawerStyle"
+import paperStyle from "./paperStyle"
 
 const componentsStyles = {
+  MuiPaper: paperStyle,
   MuiAppBar: appBarStyle,
   MuiDrawer: drawerStyle,
   MuiCssBaseline: cssBaseline,
