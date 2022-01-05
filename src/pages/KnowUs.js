8602ff26abe68986914d3fbb50572ffca1aba4d9
matteo2437 <matteo.budriesi@gmail.com>
2022-01-05 16:12:50 +0100

    update know us page

--- src/pages/KnowUs.js
+++ src/pages/KnowUs.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
 import { Grid } from '@mui/material';
 import MainImage from '../components/MainImage';
 import BlockImageAndText from '../components/BlockImageAndText';
@@ -15,35 +14,31 @@ export default function KnowUs(_props) {
   );
 
   return (
-    <Paper sx={{
-      position: "relative",
-      background: 'linear-gradient(#001466,  #091549)',
-    }}>
-      <Grid container sx={{ marginTop: 9 }}>
-        <MainImage src="./illustrations/team.svg" />
-        <CenteredText
-          variant={matches ? "h6" : "subtitle1"}
-          text="Siamo un gruppo di tre ragazzi che mangiano troppo pokè e non seguono le lezioni."
-        />
-        <CenteredTitle text="Perché sceglierci" />
-        <Grid
-          xs={12}
-          sx={{
-            maxWidth: "1350px !important",
-            margin: "0 auto",
-            "& > *": {
-              padding: 1,
-            },
-          }}
-          justifyContent="center"
-          container
-          direction="row"
-        >
-          <BlockImageAndText icon={Programming}></BlockImageAndText>
-          <BlockImageAndText icon={Personalization}></BlockImageAndText>
-          <BlockImageAndText icon={Speed}></BlockImageAndText>
-        </Grid>
+    <Grid container sx={{ marginTop: 9 }}>
+      <MainImage src="./illustrations/team.svg" />
+      <CenteredText
+        variant={matches ? "h6" : "subtitle1"}
+        text="Siamo un gruppo di tre ragazzi che mangiano troppo pokè e non seguono le lezioni."
+      />
+      <CenteredTitle text="Perché sceglierci" />
+      <Grid
+        container
+        item
+        xs={12}
+        sx={{
+          maxWidth: "1350px !important",
+          margin: "0 auto",
+          "& > *": {
+            padding: 1,
+          },
+        }}
+        justifyContent="center"
+        direction="row"
+      >
+        <BlockImageAndText icon={Programming}></BlockImageAndText>
+        <BlockImageAndText icon={Personalization}></BlockImageAndText>
+        <BlockImageAndText icon={Speed}></BlockImageAndText>
       </Grid>
-    </Paper>
+    </Grid>
   );
 }
