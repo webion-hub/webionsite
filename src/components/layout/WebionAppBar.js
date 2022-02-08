91e38b57597353add10b492ca53536fd31a274b5
AlessandroDodi <dodialessandro3@gmail.com>
2022-02-08 10:03:12 +0100

    Create animation on Contact us button

--- src/components/layout/WebionAppBar.js
+++ src/components/layout/WebionAppBar.js
@@ -1,12 +1,13 @@
 import { Button } from '@mui/material';
 import { useNavigate } from "react-router";
-
+import '../animations/growRotate.css';
 import SideBarButton from "../side_bar/SidebarButton";
 import AnimatedLogo from '../buttons/AnimatedLogo';
 
 import AppBar from "../app_bar/AppBar";
 import AppBarContent from "../app_bar/AppBarContent";
 import AppBarSection from "../app_bar/AppBarSection";
+import Img from '../Img';
 
 
 export default function WebionAppBar() {
@@ -40,11 +41,23 @@ export default function WebionAppBar() {
         <AppBarSection alignment="end">
           <AppBarSection hideOnMobile>
             <Button
+              className=""
               color="secondary"
+              sx={{
+                paddingRight: "34px",
+              }}
               variant="outlined"
               onClick={_ => navigate("/contattaci")}
             >
-              Contattaci
+              Contattaci  
+              <i className="hvr-grow-rotate">
+                <Img sx={{
+                  bottom: "-2px",
+                  width: "17px",
+                  marginBottom: "-4px",
+                }}                
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/285/telephone-receiver_1f4de.png" />
+              </i>
             </Button>
           </AppBarSection>
           <SideBarButton/>
