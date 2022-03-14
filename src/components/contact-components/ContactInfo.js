91d45a8313598e2b640d0fe16100b54cd373685c
matteo2437 <matteo.budriesi@gmail.com>
2022-03-14 09:09:52 +0100

    update footer

--- src/components/contact-components/ContactInfo.js
+++ src/components/contact-components/ContactInfo.js
@@ -2,6 +2,7 @@ import { Divider, Grid } from "@mui/material";
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded';
 import ContactItem from "./ContactItem";
+import { openEmail } from "../../lib/openContact";
 
 
 export default function ContactInfo() {
@@ -16,15 +17,14 @@ export default function ContactInfo() {
         Icon={EmailRoundedIcon}
         label="amministrazione@webion.it"
         onClick={() => {
-          const email = 'amministrazione@webion.it';
-          document.location = "mailto:"+email
+          openEmail('amministrazione@webion.it')
         }}
       />
       <ContactItem
         Icon={LocalPhoneRoundedIcon}
         label="+39 389 008 6632"
         onClick={() => {
-          window.open('tel:+39 389 008 6632');
+          openEmail('+39 389 008 6632')
         }}
       />
     </Grid>
